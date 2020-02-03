634c70dee68eedd55e706d7c82e4b97b92607e12
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2020-02-03 03:13:24 -0300

    Add first tests

--- test/twitter_clj/core_test.clj
+++ test/twitter_clj/core_test.clj
@@ -1,7 +1,32 @@
 (ns twitter-clj.core-test
-  (:require [clojure.test :refer :all]
-            [twitter-clj.core :refer :all]))
+  (:require [clojure.set :as set]
+            [clojure.test :refer :all]
+            [twitter-clj.core :refer :all])
+  (:import (java.util UUID)
+           (java.time ZonedDateTime)))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn random-uuid [] (UUID/randomUUID))
+(defn now [] (ZonedDateTime/now))
+
+(defmacro equal-except-for
+  [expected actual & exceptions]
+  `(let [keys-set# (comp set keys)
+         expected-keys# (keys-set# ~expected)
+         actual-keys# (keys-set# ~actual)
+         exception-keys# (set '~exceptions)
+         valid-keys# (set/difference (set/intersection expected-keys# actual-keys#) exception-keys#)]
+     (every? identity (vec (doall (map #(is (= (% ~expected) (% ~actual))) valid-keys#))))))
+
+(deftest like-increase-by-one
+  (testing "When like, then should increase likes by one."
+    (let [tweet (->Tweet (random-uuid) (random-uuid) "This is my tweet" (now) 10 0 0 (random-uuid))
+          liked-tweet (like tweet)]
+      (equal-except-for tweet liked-tweet :likes)
+      (is (= 11 (:likes liked-tweet))))))
+
+(deftest unlike-decrease-by-one
+  (testing "When unlike, then should decrease likes by one."
+    (let [tweet (->Tweet (random-uuid) (random-uuid) "This is my tweet" (now) 10 0 0 (random-uuid))
+          unliked-tweet (unlike tweet)]
+      (equal-except-for tweet unliked-tweet :likes)
+      (is (= 10 (:likes (unlike tweet)))))))
