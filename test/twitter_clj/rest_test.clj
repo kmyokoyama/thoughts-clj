7149b01f19c538b3c8cda4297a73f897bc0bc13b
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2020-02-11 23:12:44 -0300

    Improve tests code

--- test/twitter_clj/rest_test.clj
+++ test/twitter_clj/rest_test.clj
@@ -2,7 +2,8 @@
   (:require [twitter-clj.rest.handler :refer [handler]]
             [clojure.test :refer :all]
             [ring.server.standalone :as s]
-            [clj-http.client :as http]))
+            [clj-http.client :as client]
+            [clojure.data.json :as json]))
 
 (def server (atom nil))
 
@@ -16,12 +17,34 @@
   (.stop @server)
   (reset! server nil))
 
-(use-fixtures :once (fn [f] (start-server 3000) (f) (stop-server)))
+(def url "http://localhost:3000/")
 
-(def url "http://localhost:3000/users")
+(defn- resource
+  [path]
+  (str url "/" path))
 
-(deftest get-ok
-  (testing "Returns ok"
-    (is (let [response (http/get url {})]
-          (println response)
-          true))))
+(defn- body-as-json [{:keys [body]}]
+  (if (string? body)
+    (json/read-str body :key-fn keyword)
+    body))
+
+(defn- new-user
+  [name email nickname]
+  {:name name :email email :nickname nickname})
+
+(use-fixtures :each (fn [f] (start-server 3000) (f) (stop-server)))
+
+(deftest get-users-successfully
+  (testing "Get two users successfully"
+    ;; Given.
+    (client/get (resource "user") {:query-params (new-user "First User" "first@user.com" "first")})
+    (client/get (resource "user") {:query-params (new-user "Second User" "second@user.com" "second")})
+    ;; Then.
+    (let [response (client/get (resource "users") {})]
+      (is (= "success" (:status (body-as-json response))))
+      (is (= 2 (count (:result (body-as-json response))))))))
+
+(deftest add-single-user
+  (testing "Add a single user"
+    (let [response (client/get (resource "user") {:query-params (new-user "First User" "first@user.com" "first")})]
+      (is (= "success" (:status (body-as-json response)))))))
