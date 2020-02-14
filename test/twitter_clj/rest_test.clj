0e5a719ac9d5f03a3dfb1903197446050c5f6f33
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2020-02-14 20:13:58 -0300

    Add tweet API test

--- test/twitter_clj/rest_test.clj
+++ test/twitter_clj/rest_test.clj
@@ -34,7 +34,16 @@
   [name email nickname]
   {:name name :email email :nickname nickname})
 
+(defn- new-tweet
+  [user-id text]
+  {:user-id user-id :text text})
+
 (use-fixtures :each (fn [f] (start-server 3000) (f) (stop-server)))
+
+(deftest add-single-user
+  (testing "Add a single user"
+    (let [response (client/get (resource "user") {:query-params (new-user "First User" "first@user.com" "first")})]
+      (is (= "success" (:status (body-as-json response)))))))
 
 (deftest get-users-successfully
   (testing "Get two users successfully"
@@ -46,7 +55,16 @@
       (is (= "success" (:status (body-as-json response))))
       (is (= 2 (count (:result (body-as-json response))))))))
 
-(deftest add-single-user
-  (testing "Add a single user"
-    (let [response (client/get (resource "user") {:query-params (new-user "First User" "first@user.com" "first")})]
-      (is (= "success" (:status (body-as-json response)))))))
+(deftest add-single-tweet
+  (testing "Add a single tweet"
+    (let [user (client/get (resource "user") {:query-params (new-user "First User" "first@user.com" "first")})
+          user-id (get-in (body-as-json user) [:result :id])
+          text "My first tweet"
+          response (client/get (resource "tweet") {:query-params (new-tweet user-id text)})
+          body (body-as-json response)
+          result (:result body)]
+      (println result)
+      (is (= "success" (:status body)))
+      (is (= user-id (:user-id result)))
+      (is (= text (:text result)))
+      (is (= 0 (:likes result) (:retweets result) (:replies result))))))
