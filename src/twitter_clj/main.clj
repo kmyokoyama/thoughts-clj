5fa44aeaace3d151203431a22bdc73b1449447ec
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2020-02-15 21:45:39 -0300

    Refactor REST app entrypoint

--- src/twitter_clj/main.clj
+++ src/twitter_clj/main.clj
@@ -1,126 +1,16 @@
 (ns twitter-clj.main
-  (:require [org.httpkit.server :as server]
+  (:require [twitter-clj.rest.handler :refer [handler]]
+            [org.httpkit.server :as server]
             [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer :all]
-            [clojure.pprint :as pp]
-            [clojure.string :as str]
             [clojure.data.json :as json]
             [twitter-clj.operations :as app])
   (:gen-class))
 
-(defn get-parameter
-  [req param]
-  (param (:params req)))
-
-(def people-collection (atom []))
-
-(defn add-person
-  [firstname surname]
-  (swap! people-collection conj {:firstname (str/capitalize firstname)
-                                 :surname (str/capitalize surname)}))
-
-(add-person "Kazuki" "Yokoyama")
-
-(defn simple-body-page
-  [_req]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "Hello world"})
-
-(defn request-example
-  [req]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (->>
-           (pp/pprint req)
-           (str "Request object: " req))})
-
-(defn hello-name
-  [req]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (->
-           (pp/pprint req)
-           (str "Hello " (get-parameter req :name)))})
-
-(defn people-handler
-  [req]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (str (json/write-str @people-collection))})
-
-(defn add-person-handler
-  [req]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (->
-           (let [p (partial get-parameter req)]
-             (str (json/write-str (add-person (p :firstname) (p :surname))))))})
-
-(defn ok-json
-  [body]
-  {:status 200
-   :headers {"Content-Type" "application/json"}
-   :body body})
-
-(def success-response
-  {:status "success"})
-
-(def failure-response
-  {:status "failure"})
-
-(defn add-response-info
-  [info]
-  (assoc success-response :result info))
-
-(defn to-json
-  [r]
-  (json/write-str r :value-fn app/value-writer))
-
-(def respond-with (comp ok-json to-json add-response-info))
-
-(defn add-user
-  [req]
-  (let [name (get-parameter req :name)
-        email (get-parameter req :email)
-        nickname (get-parameter req :nickname)
-        user (app/add-user name email nickname)]
-    (respond-with user)))
-
-(defn get-users
-  [_req]
-  (let [users (app/get-users)]
-    (respond-with users)))
-
-(defn add-tweet
-  [req]
-  (let [user-id (get-parameter req :user-id)
-        text (get-parameter req :text)
-        tweet (app/add-tweet user-id text)]
-    (respond-with tweet)))
-
-(defn get-tweets-by-user
-  [req]
-  (let [user-id (get-parameter req :user-id)
-        tweets (app/get-tweets-by-user user-id)]
-    (respond-with tweets)))
-
-(defroutes app-routes
-           (GET "/" [] simple-body-page)
-           (GET "/user" [] add-user)
-           (GET "/users" [] get-users)
-           (GET "/tweet" [] add-tweet)
-           (GET "/tweets" [] get-tweets-by-user)
-           (GET "/hello" [] hello-name)
-           (GET "/people" [] people-handler)
-           (GET "/add-people" [] add-person-handler)
-           (route/not-found "Error, page not found!"))
-
 (defn -main
   [& _args]
   (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
-    (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
-    ;(server/run-server #'app-routes {:port port})
+    (server/run-server (wrap-defaults #'handler/app-routes site-defaults) {:port port})
     (println (str "Running server at http://127.0.01:" port "/"))))
 
--- src/twitter_clj/rest/handler.clj
+++ src/twitter_clj/rest/handler.clj
@@ -1,15 +1,67 @@
 (ns twitter-clj.rest.handler
-  (:require [twitter-clj.main :as main]
-            [compojure.core :refer :all]
+  (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [clojure.data.json :as json]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [twitter-clj.operations :as app]))
+
+(defn get-parameter
+  [req param]
+  (param (:params req)))
+
+(defn ok-json
+  [body]
+  {:status 200
+   :headers {"Content-Type" "application/json"}
+   :body body})
+
+(def success-response
+  {:status "success"})
+
+(def failure-response
+  {:status "failure"})
+
+(defn add-response-info
+  [info]
+  (assoc success-response :result info))
+
+(defn to-json
+  [r]
+  (json/write-str r :value-fn app/value-writer))
+
+(def respond-with (comp ok-json to-json add-response-info))
+
+(defn add-user
+  [req]
+  (let [name (get-parameter req :name)
+        email (get-parameter req :email)
+        nickname (get-parameter req :nickname)
+        user (app/add-user name email nickname)]
+    (respond-with user)))
+
+(defn get-users
+  [_req]
+  (let [users (app/get-users)]
+    (respond-with users)))
+
+(defn add-tweet
+  [req]
+  (let [user-id (get-parameter req :user-id)
+        text (get-parameter req :text)
+        tweet (app/add-tweet user-id text)]
+    (respond-with tweet)))
+
+(defn get-tweets-by-user
+  [req]
+  (let [user-id (get-parameter req :user-id)
+        tweets (app/get-tweets-by-user user-id)]
+    (respond-with tweets)))
 
 (defroutes app-routes
-           (GET "/" [] main/simple-body-page)
-           (GET "/user" [] main/add-user)
-           (GET "/users" [] main/get-users)
-           (GET "/tweet" [] main/add-tweet)
-           (GET "/tweets" [] main/get-tweets-by-user)
+           (GET "/user" [] add-user)
+           (GET "/users" [] get-users)
+           (GET "/tweet" [] add-tweet)
+           (GET "/tweets" [] get-tweets-by-user)
            (route/not-found "Error, page not found!"))
 
 (def handler
