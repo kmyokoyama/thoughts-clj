bd0bb5f84fe7be7216834e12709a56cc23be8b82
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2020-03-05 20:56:54 -0300

    Reorganize deps in project.clj

--- project.clj
+++ project.clj
@@ -6,15 +6,14 @@
   :profiles {:dev {:dependencies [[org.clojure/data.generators "0.1.2"]
                                   [cheshire "5.10.0"]
                                   [clj-http "3.10.0"]]}}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [compojure "1.6.1"]
-                 [http-kit "2.3.0"]
-                 [ring/ring-defaults "0.3.2"]
-                 [ring-server "0.4.0"]
-                 [ring/ring-json "0.5.0"]
-                 [org.clojure/data.json "0.2.6"]
+  :dependencies [[compojure "1.6.1"]
+                 [com.taoensso/timbre "4.10.0"]
                  [com.stuartsierra/component "0.4.0"]
-                 [com.taoensso/timbre "4.10.0"]]
+                 [http-kit "2.3.0"]
+                 [org.clojure/clojure "1.10.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [ring-server "0.4.0"]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-json "0.5.0"]]
   :repl-options {:init-ns twitter-clj.core}
-  :jvm-opts ["-Dclojure.tools.logging.factory=clojure.tools.logging.impl/log4j-factory"]
   :main twitter-clj.application.main)
