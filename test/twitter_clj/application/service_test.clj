a817398fcab9313fa69dd08c576c68b1113a8cc2
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2020-06-29 12:15:35 -0300

    Fix tests of service

--- test/twitter_clj/application/service_test.clj
+++ test/twitter_clj/application/service_test.clj
@@ -3,53 +3,45 @@
             [twitter-clj.application.port.repository :as repository]
             [twitter-clj.application.test-util :refer :all]
             [twitter-clj.application.service :refer :all]
-            [twitter-clj.application.core :as core]
-            [buddy.hashers :as hashers]))
+            [twitter-clj.application.port.service :refer :all]
+            [twitter-clj.application.core :as core]))
 
-(deftest new-user
+(deftest test-new-user
   (testing "new-user? returns true if no user is fetched from repository"
-    (let [stub-repository (reify repository/Repository
+    (let [stub-repository (reify repository/UserRepository
                             (fetch-users! [_ _] []))        ;; No user is fetched.
-          new? (new-user? {:repository stub-repository} (random-email))]
+          service (map->Service {:repository stub-repository})
+          new? (new-user? service (random-email))]
       (is new?)))
 
   (testing "new-user? returns false if at least one user is fetched from repository"
-    (let [stub-repository (reify repository/Repository
+    (let [stub-repository (reify repository/UserRepository
                             (fetch-users! [_ _] [(random-user)])) ;; One user is fetched.
-          new? (new-user? {:repository stub-repository} (random-email))]
+          service (map->Service {:repository stub-repository})
+          new? (new-user? service (random-email))]
       (is (not new?)))))
 
-(deftest user-exists
+(deftest test-user-exists
   (testing "user-exists? returns true if the specified user is returned from repository"
-    (let [stub-repository (reify repository/Repository
+    (let [stub-repository (reify repository/UserRepository
                             (fetch-users! [_ _] [(random-user)])) ;; One user is fetched.
-          new? (user-exists? {:repository stub-repository} (random-uuid))]
+          service (map->Service {:repository stub-repository})
+          new? (user-exists? service (random-uuid))]
       (is new?)))
 
   (testing "user-exists? returns false if no user is returned from repository"
-    (let [stub-repository (reify repository/Repository
-                            (fetch-users! [_ _] []))        ;; No user is fetched.
-          new? (user-exists? {:repository stub-repository} (random-uuid))]
+    (let [stub-repository (reify repository/UserRepository
+                            (fetch-users! [_ _] [])) ;; No user is fetched.
+          service (map->Service {:repository stub-repository})
+          new? (user-exists? service (random-uuid))]
       (is (not new?)))))
 
-(deftest password-match
+(deftest test-password-match
   (testing "password-match? returns true if password belongs to user"
     (let [password (random-password)
           hashed-password (core/derive-password password)
-          stub-repository (reify repository/Repository
+          stub-repository (reify repository/UserRepository
                             (fetch-password! [_ _] hashed-password))
-          match? (password-match? {:repository stub-repository} (random-uuid) password)]
+          service (map->Service {:repository stub-repository})
+          match? (password-match? service (random-uuid) password)]
       (is match?))))
-
-(deftest logged-in
-  (testing "logged-in? returns true if there is a session associated with the user"
-    (let [stub-repository (reify repository/Repository
-                            (fetch-sessions! [_ _] ["session"]))
-          logged? (logged-in? {:repository stub-repository} (random-uuid))]
-      (is logged?)))
-
-  (testing "logged-in? returns false if there is no session associated with the user"
-    (let [stub-repository (reify repository/Repository
-                            (fetch-sessions! [_ _] []))
-          logged? (logged-in? {:repository stub-repository} (random-uuid))]
-      (is (not logged?)))))
