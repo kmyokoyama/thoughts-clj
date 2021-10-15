da079a251a6731769bf6f672f3ef012ad07756cf
Kazuki Yokoyama <yokoyama.km@gmail.com>
2021-10-14 22:41:18 -0300

    Fix jetty server

--- src/thoughts/adapter/http/component.clj
+++ src/thoughts/adapter/http/component.clj
@@ -2,20 +2,21 @@
   (:require [com.stuartsierra.component :as component]
             [taoensso.timbre :as log]
             [thoughts.adapter.http.handler :as a.http.handler]
-            [ring.adapter.jetty :as jetty]))
+            [ring.adapter.jetty :as jetty])
+  (:import (org.eclipse.jetty.server Server)))
 
 (defrecord HttpServer [host port http-server service]
   component/Lifecycle
   (start [this]
     (log/info "Starting HTTP server on" (str "http://" host ":" port))
     (assoc this :http-server
-                (jetty/run-jetty (a.http.handler/handler service) {:ip host :port port})))
+                (jetty/run-jetty (a.http.handler/handler service) {:port port :join? false})))
 
   (stop [this]
     (log/info "Stopping HTTP server" port)
-    (let [stop-fn (:http-server this)]
-      (stop-fn)
-      this)))
+    (.stop ^Server (:http-server this))
+    this))
+
 
 (defn make-http-controller                                  ;; Constructor.
   [host port]
