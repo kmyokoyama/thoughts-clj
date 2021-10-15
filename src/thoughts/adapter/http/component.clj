f8abf2e2801a0d21870022cef62bd3f225f16e74
Kazuki Yokoyama <yokoyama.km@gmail.com>
2021-10-14 22:22:29 -0300

    Revert "Replace http.server with jetty"
    This reverts commit e015ea0e287c021cc9ea2b2714c475a6d7137e3b.

--- src/thoughts/adapter/http/component.clj
+++ src/thoughts/adapter/http/component.clj
@@ -1,15 +1,15 @@
 (ns thoughts.adapter.http.component
   (:require [com.stuartsierra.component :as component]
+            [org.httpkit.server :as http.server]
             [taoensso.timbre :as log]
-            [thoughts.adapter.http.handler :as a.http.handler]
-            [ring.adapter.jetty :as jetty]))
+            [thoughts.adapter.http.handler :as a.http.handler]))
 
 (defrecord HttpServer [host port http-server service]
   component/Lifecycle
   (start [this]
     (log/info "Starting HTTP server on" (str "http://" host ":" port))
     (assoc this :http-server
-                (jetty/run-jetty (a.http.handler/handler service) {:ip host :port port})))
+                (http.server/run-server (a.http.handler/handler service) {:ip host :port port})))
 
   (stop [this]
     (log/info "Stopping HTTP server" port)
